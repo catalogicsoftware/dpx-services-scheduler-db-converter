56f7bad7f5d6ffd7efa9c9fe73c2f500108bf1e5
Kamil Szwaj <kszwaj@catalogicsoftware.com>
2026-01-08 12:31:04 +0100

    feat(DPXN-11021): scheduler migration script from apscheduler to json dump db file

--- script/scheduler/usecases/report.py
+++ script/scheduler/usecases/report.py
@@ -3,11 +3,9 @@
 # they are only needed so APScheduler can restore jobs from the database
 
 
-def run_schedule(*args, **kwargs):
+def report_schedule(*args, **kwargs):
     """
-    Stub function for run_schedule.
+    Stub function for report_schedule.
     This function is never actually called during migration.
     """
     pass
-
-
